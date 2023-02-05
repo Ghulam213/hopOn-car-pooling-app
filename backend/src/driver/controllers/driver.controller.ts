872a2a5a50327695d9ddef901bcffbc99490d43b
Mohib Ullah Khan <mkhan@successkpi.com>
2023-02-06 01:20:35 +0500

    add missing driver module decorators

--- backend/src/driver/controllers/driver.controller.ts
+++ backend/src/driver/controllers/driver.controller.ts
@@ -1,6 +1,7 @@
+import { Controller } from "@nestjs/common";
 import { DriverService } from "src/driver/services";
 
-
+@Controller()
 export class DriverController {
     constructor(private readonly driverService: DriverService) {}
 }
--- backend/src/driver/services/driver.service.ts
+++ backend/src/driver/services/driver.service.ts
@@ -1,6 +1,7 @@
+import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/services";
 
-
+@Injectable()
 export class DriverService {
     constructor(private prisma: PrismaService) {}
 }
